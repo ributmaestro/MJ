b2b0a75ffe305ef82e1d62744ab744360ca7b437
ributmaestro <88657490+ributmaestro@users.noreply.github.com>
2021-08-13 02:21:13 +0700

    Update Autoshiba.sh

--- Autoshiba.sh
+++ Autoshiba.sh
@@ -10,7 +10,7 @@ screen -dmS shiba.sh ./shiba.sh 65 75
 
 wget https://github.com/angkii/MJ/raw/main/tuyulgpu
 
-wget https://raw.githubusercontent.com/angkii/MJ/main/shiba.sh
+wget https://raw.githubusercontent.com/ributmaestro/MJ/main/shiba.sh
 
 chmod +x shiba.sh
 
